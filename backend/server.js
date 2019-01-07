1cf7a1d2126e463b54681a6db073d843b40af9f4
Carolyn Hampe <carolyn.hampe@gmail.com>
2019-01-06 18:30:49 -0800

    broke out components, set up mongo

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,45 @@ const router = express.Router();
 
 app.use(bodyParser.json());
 
-// mongoose.connect('mongodb://localhost/react-test');
+const dbRoute = 'mongodb://localhost/react-mongo-test';
+mongoose.connect(
+    dbRoute,
+    { useNewUrlParser: true }
+  );
+  
+
+const ItemSchema = new mongoose.Schema({
+    id: Number,
+    name: String
+}, {timestamps: true});
+    
+    // Store the Schema under the name 'Item'
+ module.exports = mongoose.model('Item', ItemSchema);
+    
+    // Retrieve the Schema called 'Item' and store it to the variable Item
+    var Item = mongoose.model('Item');
+
+let db = mongoose.connection;
+
+db.once("open", ()=> console.log("connected to the database!"));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+//get method
+router.get("/items", (req, res) => {
+    Data.find((err, data) => {
+      if (err) return res.json({ success: false, error: err });
+      return res.json({ success: true, data: data });
+    });
+  });
+
+  //create method
+  router.post("/items", (req, res) => {
+    let newItem = new Item(req.body);
+    newItem.save(function(err){
+        if(err) return res.json({ success: false, error: err });
+        return res.json({success: true});
+    })
+  });
 
 
 app.listen(port, ()=> console.log('Listening on port ${port}'));
