fca33f994ba4968d9abaef7637b66f16829e2c16
Carolyn Hampe <carolyn.hampe@gmail.com>
2019-01-22 22:45:53 -0800

    set up edit, need to fix bug

--- backend/server.js
+++ backend/server.js
@@ -58,6 +58,15 @@ router.get("/items", (req, res) => {
     });
   });
 
+  //update method
+  router.post("/update", (req, res) =>{
+    const {id, update} = req.body;
+    Item.findOneAndUpdate(id, update, err => {
+      if (err) return res.json({success: false, error: err});
+      return res.json({sucess: true});
+    });
+  });
+
 app.use("/api", router);
 
 app.listen(port, ()=> console.log(`Listening on port ${port}`));
